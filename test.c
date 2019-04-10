2bca6f6d1d5b7064a84462d1ab42684ddabd8ca3
junaidsyed786 <junaid911999@gmail.com>
2019-04-10 15:45:42 +0530

    Update test.c

--- test.c
+++ test.c
@@ -1,24 +1,18 @@
-#include <stdio.h>
+void towerOfHanoi(int n, char from_rod, char to_rod, char aux_rod)
+{
+    if (n == 1)
+    {
+       // printf("\n Move disk 1 from rod %c to rod %c", from_rod, to_rod);
+        return;
+    }
+    towerOfHanoi(n-1, from_rod, aux_rod, to_rod);
+    //printf("\n Move disk %d from rod %c to rod %c", n, from_rod, to_rod);
+    towerOfHanoi(n-1, aux_rod, to_rod, from_rod);
+}
+
 int main()
 {
-    int n, i;
-    unsigned long long factorial = 1;
-
-    printf("Enter an integer: ");
-    scanf("%d",&n);
-
-    // show error if the user enters a negative integer
-    if (n < 0)
-        printf("Error! Factorial of a negative number doesn't exist.");
-
-    else
-    {
-        for(i=1; i<=n; ++i)
-        {
-            factorial *= i;              // factorial = factorial*i;
-        }
-        printf("Factorial of %d = %llu", n, factorial);
-    }
-
+    int n = 4; // Number of disks
+    towerOfHanoi(n, 'A', 'C', 'B');  // A, B and C are names of rods
     return 0;
 }
